c5e5b3ac019799c268d66a2588c5f52a40d96af4
Azlan <azlan@123>
2021-05-28 12:09:06 +0530

    Project add in GIT

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  const name = "Azlan";
+  return (
+    <div className="App">
+      <p>Hello  AAA{name} {new Date().toDateString()}</p>
+    </div>
+  );
+}
+
+export default App;
